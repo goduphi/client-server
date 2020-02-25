219b0461ed9e6a0bc4e3b473a670181edf0ab59b
goduphi <sarkernadir.azmi@mavs.uta.edu>
2020-02-24 16:27:21 -0800

    Initial commit

---
+++ SynchronousClient/SynchronousClient/Program.cs
@@ -0,0 +1,126 @@
+﻿/*
+    Author: Sarker Nadir Afridi Azmi
+*/
+
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Security;
+
+namespace SynchronousClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Client";
+
+            // This is the port number that we are going to use in order to connect to a specific application program on the host
+            int port = 11000;
+
+            // First, we need to get the IP address of the host that we are trying to connect to
+            // Resolve() return a list of all the ip addresses and aliases for the requested name
+            IPHostEntry ipHostInfo = Dns.Resolve(Dns.GetHostName());
+
+            // We can access the information returned by Resolve() as it essentially returns
+            // information in an array called the address list
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+
+            // We need to create a data pipe between the client and remote device
+            // Essentially, we need to create a remote end point that the client can talk to
+            IPEndPoint ipe = new IPEndPoint(ipAddress, port);
+
+            // This socket establishes a connection between the client and remote device
+            Socket s = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                s.Connect(ipe);
+            }
+            catch(ArgumentNullException ae)
+            {
+                Console.WriteLine("ArgumentNullException {0}", ae.ToString());
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine("SocketException {0}", se.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("ArgumentNullException {0}", e.ToString());
+            }
+
+            string message = "This is a test string.";
+
+            // Information over a network can only be sent in bytes
+            // Convert the string message into a byte array using the Encoding.ASCII property
+            byte[] msg = System.Text.Encoding.ASCII.GetBytes(message + "<EOF>");
+            int bytesSent;
+
+            try
+            {
+                bytesSent = s.Send(msg);
+            }
+            catch (ArgumentNullException ae)
+            {
+                Console.WriteLine("ArgumentNullException {0}", ae.ToString());
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine("SocketException {0}", se.ToString());
+            }
+            catch (ObjectDisposedException ode)
+            {
+                Console.WriteLine("ObjectDisposedException {0}", ode.ToString());
+            }
+
+            // Information can only be received in bytes
+
+            byte[] msgRecv = new byte[1024];
+            int bytesRecv;
+
+            try
+            {
+                bytesRecv = s.Receive(msgRecv);
+            }
+            catch (ArgumentNullException ae)
+            {
+                Console.WriteLine("ArgumentNullException {0}", ae.ToString());
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine("SocketException {0}", se.ToString());
+            }
+            catch (ObjectDisposedException ode)
+            {
+                Console.WriteLine("ObjectDisposedException {0}", ode.ToString());
+            }
+            catch (SecurityException se)
+            {
+                Console.WriteLine("SecurityException {0}", se.ToString());
+            }
+
+            // Use the Encoding.ASCII property to convert the bytes back into a string
+            // Assumption: Data coming from the network is ASCII encoded text
+            string msgRecvString = System.Text.Encoding.ASCII.GetString(msgRecv);
+            Console.WriteLine("Echoed back from server: {0}", msgRecvString);
+
+            // Shutdown the socket for both sending and receiving and close the connection
+
+            try
+            {
+                s.Shutdown(SocketShutdown.Both);
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine("SocketException {0}", se.ToString());
+            }
+            catch (ObjectDisposedException ode)
+            {
+                Console.WriteLine("ObjectDisposedException {0}", ode.ToString());
+            }
+
+            s.Close();
+        }
+    }
+}
---
+++ SynchronousServer/SynchronousServer/Program.cs
@@ -0,0 +1,92 @@
+﻿/*
+    Author: Sarker Nadir Afridi Azmi
+*/
+
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Security;
+
+namespace SynchronousServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Server";
+
+            // Max number of incoming connections
+            int maxNumberOfConnections = 4;
+
+            // This is the port that will be used by the client to identify this server
+            int port = 11000;
+
+            // Listener or server sockets open a port on the network and then wait for a client to connect to that port
+            // Resolve() returns information of all the addresses and aliases to an array called the AddressList
+            // We will be using the first address returned
+            IPHostEntry ipHostinfo = Dns.Resolve(Dns.GetHostName());
+            IPAddress ipAddress = ipHostinfo.AddressList[0];
+
+            // Now what we need to do is to create an end point of the service that is being provided
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, port);
+
+            // Now we have to associate the endpoint we created with a socket
+            Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            Socket handler = null;
+
+            try
+            {
+                listener.Bind(localEndPoint);
+
+                // Listen for a client endpoint
+                listener.Listen(maxNumberOfConnections);
+
+                Console.WriteLine("Waiting to connect to a client...");
+
+                // Accept incoming connections from the client
+                handler = listener.Accept();
+
+                byte[] msgFromClinet = new byte[1024];
+                int bytesRecv = 0;
+                string data = null;
+
+                while(true)
+                {
+                    bytesRecv = handler.Receive(msgFromClinet);
+                    data += System.Text.Encoding.ASCII.GetString(msgFromClinet);
+                    if(data.IndexOf("<EOF>") > -1)
+                    {
+                        break;
+                    }
+                }
+
+                Console.WriteLine("Text received : {0}", data);
+
+                // Encode the string in ASCII encoded bytes because you can only send bytes over the network
+                byte[] msgEchoToclient = System.Text.Encoding.ASCII.GetBytes(data);
+                handler.Send(msgEchoToclient);
+
+            }
+            catch (ArgumentNullException ae)
+            {
+                Console.WriteLine("ArgumentNullException {0}", ae.ToString());
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine("SocketException {0}", se.ToString());
+            }
+            catch (ObjectDisposedException ode)
+            {
+                Console.WriteLine("ObjectDisposedException {0}", ode.ToString());
+            }
+            catch (SecurityException se)
+            {
+                Console.WriteLine("SecurityException {0}", se.ToString());
+            }
+
+            handler.Shutdown(SocketShutdown.Both);
+            handler.Close();
+
+        }
+    }
+}
